52ee932c23735a6bdc1ef10fcec453b754cec222
Aniket Bulbule <abulbul@amazon.com>
2023-07-18 10:13:22 -0400

    Deleting tfstate

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "buses" {
   type        = list(string)
-  default     = ["arn:aws:events:us-east-1:768827764971:event-bus/Inventory", "arn:aws:events:us-east-1:768827764971:event-bus/Orders", "arn:aws:events:us-west-2:768827764971:event-bus/eventBus1"]
+  default     = ["arn:aws:events:us-east-1:768827764971:event-bus/eb1", "arn:aws:events:us-east-1:768827764971:event-bus/eb2"]
   description = "Name of the first EventBus"
 }
 
